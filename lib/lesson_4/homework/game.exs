1bf15ef7a2959fbc260f1cecee43e4ab5b736912
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2025-02-26 22:55:30 +0500

    hw 4 complite

--- lib/lesson_4/homework/game.exs
+++ lib/lesson_4/homework/game.exs
@@ -1,17 +1,31 @@
 defmodule Game do
-  def join_game(user) do
-    # TODO add your implementation
+  def join_game({:user, name, age, role}) do
+    case {age, role} do
+      {_, :admin} -> :ok
+      {_, :moderator} -> :ok
+      {age, _} when age >= 18 -> :ok
+      _ -> :error
+    end
   end
 
   def move_allowed?(current_color, figure) do
-    # TODO add your implementation
+    case figure do
+      {:pawn, color} when current_color == color -> true
+      {:rock, color} when current_color == color -> true
+      _ -> false
+    end
   end
 
   def single_win?(a_win, b_win) do
-    # TODO add your implementation
+    a_win != b_win
   end
 
   def double_win?(a_win, b_win, c_win) do
-    # TODO add your implementation
+    case {a_win, b_win, c_win} do
+      {true, true, false} -> :ab
+      {true, false, true} -> :ac
+      {false, true, true} -> :bc
+      _ -> false
+    end
   end
 end
